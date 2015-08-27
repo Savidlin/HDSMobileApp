e6a0a632d8957b9786f4963d5ba0cb3b78c03b30
Joseph Moran <josephmoran@live.com>
2015-08-27 16:08:15 -0400

    added loading signal and removed old test demo

---
+++ HDSMobileApp/app/dist/loading.js
@@ -0,0 +1,16 @@
+﻿$(document).ajaxStart(function () {
+    $(window.document.body).append($(
+        "<div class='container loading'>" +
+            "<div class='col-md-2 col-md-offset-5 text-center'>" +
+                "Loading. Please Wait." +
+                "<br /> <img class='img-responsive' src='/app/images/loading.gif' height='10'>" +
+            "</div>" +
+        "</div>"
+        ));
+
+    $("#content").hide();
+
+    }).ajaxStop(function () {
+        $(".loading").remove();
+        $("#content").show();
+    });
